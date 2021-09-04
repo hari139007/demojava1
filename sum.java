3c1760c5a944e74b8ccf561c1fadaea87bddf136
hari139007 <87061286+hari139007@users.noreply.github.com>
2021-09-04 19:09:11 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,11 +1,10 @@
-public class sum
-{
-public static void main(String args[])
-{
-int num1=34,num2=45,product;
-sum=num1*num2;
-System.out.println("The sum is: ",+sum);
+public class sum {
 
-}
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
 
+      System.out.println("Sum of these numbers: "+sum);
+   }
 }
